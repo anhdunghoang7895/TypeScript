4476665bcef7a6f93fd93ebcb8b036ce950eef85
anhdunghoang7895 <anhdunghoang7895@gmail.com>
2019-04-01 14:23:54 +0700

    update

---
+++ mjs/TH-Array.js
@@ -0,0 +1,13 @@
+const list = [1, 2, 3];
+const categories = ['Sport', 'IT', 'Car'];
+console.log('list');
+list.forEach((num) => console.log(num.toFixed(2)));
+console.log('categries');
+categories.forEach((str) => console.log(str.includes('a')));
+const listSquare = list.map(num => num * num);
+console.log(listSquare);
+const result = categories.filter(str => str.length > 2);
+console.log(result);
+let x;
+x = ["hello", 10];
+console.log(x[0].substr(1));
